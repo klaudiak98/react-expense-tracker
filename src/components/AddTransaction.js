c742435d99316c18421ee2e3c34c3411561c794e
Klaudia Kieryk <klaudiakieryk123@gmail.com>
2021-08-24 23:06:48 +0200

    Adding global state and updating TransactionList component

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,15 +1,21 @@
+import {useState} from 'react'
+
 const AddTransaction = () => {
+
+    const [text,setText] = useState('');
+    const [amount,setAmount] = useState(0);
+
     return (
         <>
             <h3>Add new transaction</h3>
             <form>
                 <div className='form-control'>
                     <label htmlFor='text'>Text</label>
-                    <input type='text' placeholder='Enter text...'></input>
+                    <input type='text' value={text} onChange={(e) => setText(e.target.value)} placeholder='Enter text...'></input>
                 </div>
                 <div className='form-control'>
                     <label htmlFor='amount'>Amount <br/>(negative - expense, positive - income)</label>
-                    <input type='number' placeholder='Enter amount...'></input>
+                    <input type='number' value={amount} onChange={(e) => setAmount(e.target.value)} placeholder='Enter amount...'></input>
                 </div>
                 <button className='btn'>Add transaction</button>
             </form>
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,11 +1,17 @@
+import { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+import Transaction from './Transaction';
+
 const TransactionList = () => {
+    const { transactions } = useContext(GlobalContext);
+
+
     return (
         <>
             <h3>History</h3>
             <ul className='list'>
-                <li className = 'minus'>
-                    Cach <span>-$400</span><button className='delete-btn'>x</button>
-                </li>
+                { transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction}/> )) }
+               
             </ul>
         </>
     )
