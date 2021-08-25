b23c2ef38080e65d43bdeb63a4050814b2fa6ec4
Klaudia Kieryk <klaudiakieryk123@gmail.com>
2021-08-25 20:33:02 +0200

    Adding option to delete transaction

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,10 +1,15 @@
+import { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
 const Transaction = ({ transaction }) => {
+    const { deleteTransaction } = useContext(GlobalContext);
+
     const sign = transaction.amount < 0 ? '-' : '+'
 
     return (
-        <li className = 'minus'>
+        <li className = {transaction.amount < 0 ? 'minus' : 'plus'}>
             {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
-            <button className='delete-btn'>x</button>
+            <button className='delete-btn' onClick={()  => deleteTransaction(transaction.id)}>x</button>
         </li>
     )
 }
